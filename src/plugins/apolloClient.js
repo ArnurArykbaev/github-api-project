ca91fb87c9209636435c2349d016c702b3dc61fe
Arnur <arnurarykbayev@mail.ru>
2023-06-13 22:53:17 +0600

    just two strings

--- src/plugins/apolloClient.js
+++ src/plugins/apolloClient.js
@@ -12,7 +12,7 @@ function getHeaders() {
   console.log("OK")
   const headers = {};
   const token = process.env.VITE_API_TOKEN;
-  console.log("OK2", token)
+  console.log("OK2", `Bearer ${'ghp_qmp66lRlXDCkYLgv'}0rDFwB1NyibvOK3zKh3U`)
   if (token) {
     headers["Authorization"] = `Bearer ${'ghp_qmp66lRlXDCkYLgv'}0rDFwB1NyibvOK3zKh3U`;
   }
